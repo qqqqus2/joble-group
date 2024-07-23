a4a7cee78133194adacba7392034c0adca70127a
bryan <bryan@kindpeople.co.kr>
2024-07-23 23:55:08 +0900

    작업중

--- html/include/header.js
+++ html/include/header.js
@@ -2,7 +2,7 @@ function includeHeader() {
   let str = `
 <header id="header" class="header">
   <div class="inner">
-    <h1 id="logo"><a href="../html/main.html" aria-label="조블페이">조블페이</a></h1>
+    <h1 id="logo"><a href="../html/main.html" aria-label="조블 컴퍼니">JOBLE COMPANY</a></h1>
     <nav id="gnb">
       <button type="button" class="btn-gnb">
         <span class="gnb-current">모바일 메뉴 펼치기</span>
@@ -12,7 +12,6 @@ function includeHeader() {
         <li><a href="../html/reference.html" aria-label="JOBLEPIN">JOBLEPIN</a></li>
         <li><a href="../html/esg_environment.html" aria-label="ENTERTAINMENT">ENTERTAINMENT</a></li>
         <li><a href="../html/cs_center_info.html" aria-label="SERVICE">SERVICE</a></li>
-        <li><a href="../html/aboutus_greeting.html" aria-label="개인정보처리방침">개인정보처리방침</a></li>
       </ul>
     </nav>
   </div>
